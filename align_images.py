9dda5a890f03187d50c444bfea10399b03461614
rohanBrid18 <bridrohan1122@gmail.com>
2020-09-07 00:36:09 +0530

    added align_images function

--- align_images.py
+++ align_images.py
@@ -1,3 +1,7 @@
 import numpy as np
 import imutils
 import cv2
+
+def align_images(image, template, maxFeatures=500, keepPercent=0.2, debug=False):
+    imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
