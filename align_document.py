10c14b3fbb2318e81b3e1ade00ab89b725d56dca
rohanBrid18 <bridrohan1122@gmail.com>
2020-09-08 01:10:27 +0530

    stacked and overlay output

--- align_document.py
+++ align_document.py
@@ -19,3 +19,16 @@ template = cv2.imread(args["template"])
 
 print("[Info] aligning images...")
 aligned = align_images.align_images(image, template, debug=args["debug"])
+
+aligned = imutils.resize(aligned, width=700)
+template = imutils.resize(template, width=700)
+
+stacked = np.hstack([aligned, template])
+
+overlay = template.copy()
+output = aligned.copy()
+cv2.addWeighted(overlay, 0.5, output, 0.5, 0, output)
+
+cv2.imshow("Image Alignment: Stacked", stacked)
+cv2.imshow("Image Alignment: Overlay", output)
+cv2.waitKey(0)
