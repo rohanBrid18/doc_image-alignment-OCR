8bab0be55520783e8a1fc5cf5a005fa48c3e2e20
rohanBrid18 <bridrohan1122@gmail.com>
2020-09-07 21:23:09 +0530

    keypoints matched

--- align_images.py
+++ align_images.py
@@ -5,3 +5,22 @@ import cv2
 def align_images(image, template, maxFeatures=500, keepPercent=0.2, debug=False):
     imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    
+    orb = cv2.ORB_create(maxFeatures)
+    (kpsA, descsA) = orb.detectAndCompute(imgGray, None)
+    (kpsB, descsB) = orb.detectAndCompute(templateGray, None)
+    
+    method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
+    matcher = cv2.DescriptorMatcher_create(method)
+    matches = matcher.match(descsA, descsB, None)
+
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    keep = int(len(matches) * keepPercent)
+    matches = matches[:keep]
+
+    if debug:
+        matchedVis = cv2.drawMatches(image, kpsA, template, kpsB, matches, None)
+        matchedVis = imutils.resize(matchedVis, width=1000)
+        cv2.imshow("Matched Keypoints", matchedVis)
+        cv2.waitKey(0)
