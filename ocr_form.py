f8598573b71c12b348de69bdaa878b148a091bb8
rohanBrid18 <bridrohan1122@gmail.com>
2020-09-09 23:40:08 +0530

    display results

--- ocr_form.py
+++ ocr_form.py
@@ -16,7 +16,7 @@ OCRLocation = namedtuple("OCRLocation", ["id", "bbox", "filter_keywords"])
 
 OCR_Locations = [
     OCRLocation("step1_first_name", (58, 49, 150, 20),   #(265, 237, 751, 106)
-        ["middle", "initial", "first", "name"]),
+        []),
     # OCRLocation("step1_last_name", (1020, 237, 835, 106),
 	# 	["last", "name"]),
 	# OCRLocation("step1_address", (265, 336, 1588, 106),
@@ -40,8 +40,6 @@ template = cv2.imread(args["template"])
 
 print("[Info] aligning images...")
 aligned = align_images.align_images(image, template)
-cv2.imshow("Aligned", aligned)
-cv2.waitKey(0)
 
 print("[Info] OCR'ing document...")
 parsingResults = []
@@ -49,7 +47,7 @@ parsingResults = []
 for loc in OCR_Locations:
     (x, y, w, h) = loc.bbox
     roi = aligned[y:y+h, x:x+w]
-    cv2.imshow("roi", roi)
+    cv2.imshow(loc.id, roi)
     cv2.waitKey(0)
     
     rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
@@ -65,4 +63,35 @@ for loc in OCR_Locations:
         if count == 0:
             parsingResults.append((loc, line))
 
-print("Result:", parsingResults)
+results = {}
+
+for (loc, line) in parsingResults:
+    r = results.get(loc.id, None)
+
+    if r is None:
+        results[loc.id] = (line, loc._asdict())
+    
+    else:
+        (existingText, loc) = r
+        text = "{}\n{}".format(existingText, line)
+
+        results[loc["id"]] = (text, loc)
+
+for (locID, result) in results.items():
+    (text, loc) = result
+
+    print(loc["id"])
+    print("=" * len(loc["id"]))
+    print("{}\n\n".format(text))
+
+    (x, y, w, h) = loc["bbox"]
+
+    cv2.rectangle(aligned, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    for (i, line) in enumerate(text.split("\n")):
+        startY = y + (i * 70) + 40
+        cv2.putText(aligned, line, (x, startY), cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 255), 5)
+
+cv2.imshow("Input", imutils.resize(image, width=700))
+cv2.imshow("Output", imutils.resize(aligned, width=700))
+cv2.waitKey(0)
